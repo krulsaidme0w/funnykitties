0a7953c5be7a9430ac4a83a5dc512eb9477e83a3
krulsaidme0w <mr.prilutskiy.2002@mail.ru>
2021-10-29 19:25:05 +0300

    button facade

---
+++ src/button/button.h
@@ -0,0 +1,15 @@
+#include "SFML/Graphics/Drawable.hpp"
+
+namespace gui {
+
+
+    class Button : public sf::Drawable {
+      public:
+        Button();
+        ~Button();
+        void update(sf::Event& e, sf::RenderWindow& window);
+      private:
+
+    };
+
+}
