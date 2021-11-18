931a5c943f8d676297660b65b79639bc67d56340
Nikita Schelkanov <nik.schelkanov@gmail.com>
2021-11-18 17:15:27 +0300

    created code for server

--- network/src/Server/Server.cpp
+++ network/src/Server/Server.cpp
@@ -1,4 +1,20 @@
 namespace network {
-    
-    
+            Server::Server(uint16_t nPort) : network::ServerInterface(nPort) {
+            }
+
+            bool Server::OnClientConnect(std::shared_ptr<network::connection<GameMsg>> client) override {
+                return true;
+            }
+
+            void Server::OnClientValidated(std::shared_ptr<network::connection<GameMsg>> client) override {
+
+            }
+
+            void Server::OnClientDisconnect(std::shared_ptr<network::connection<GameMsg>> client) override {
+
+            }
+
+            void Server::OnMessage(std::shared_ptr<network::connection<GameMsg>> client, network::types::message<GameMsg>& msg) override {
+            }
+
 } // namespace network
