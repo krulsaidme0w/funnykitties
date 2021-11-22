a76c36f90a69ac8eb5d37a840e8a8f19ec1014a7
krulsaidme0w <mr.prilutskiy.2002@mail.ru>
2021-11-22 04:09:37 +0300

    gameobj

--- src/gameobject/gameobject.cpp
+++ src/gameobject/gameobject.cpp
@@ -1,1 +1,31 @@
+#include "gameobject.h"
 
+GameObject::GameObject::GameObject() {
+}
+
+GameObject::GameObject::~GameObject() {
+}
+
+void GameObject::GameObject::SetCoordinates(sf::Vector2f coordinates) {
+    this->sprite.setPosition(coordinates);
+}
+
+sf::Vector2f GameObject::GameObject::GetCoordinates() {
+    return this->sprite.getPosition();
+}
+
+void GameObject::GameObject::SetSprite(sf::Sprite sprite) {
+    this->sprite = sprite;
+}
+
+sf::Sprite GameObject::GameObject::GetSprite() {
+    return this->sprite;
+}
+
+void GameObject::GameObject::SetTextue(sf::Texture) {
+    this->texture = texture;
+}
+
+sf::Texture GameObject::GameObject::GetTexture() {
+    return this->texture;
+}
--- src/gameobject/gameobject.h
+++ src/gameobject/gameobject.h
@@ -1,1 +1,25 @@
 #pragma once
+
+#include "iostream"
+#include "SFML/Graphics.hpp"
+
+namespace GameObject {
+      class GameObject {
+      public:
+          explicit GameObject();
+          virtual ~GameObject();
+
+          void SetCoordinates(sf::Vector2f coordinates);
+          sf::Vector2f GetCoordinates();
+
+          void SetSprite(sf::Sprite sprite);
+          sf::Sprite GetSprite();
+
+          void SetTextue(sf::Texture texture);
+          sf::Texture GetTexture();
+
+      protected:
+          sf::Sprite sprite;
+          sf::Texture texture;
+      };
+}
