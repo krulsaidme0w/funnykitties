2fec5090b8520350dfe424690bbca7f047de8c28
sonichka-s <stnchnk0@gmail.com>
2021-12-06 01:20:41 +0300

    update buttons with fixed text

--- src/buttons/button.cpp
+++ src/buttons/button.cpp
@@ -13,20 +13,21 @@ GUI::Button::Button(float x, float y, std::string text, std::string pathFont, st
 
     this->sprite.setTexture(this->idleTexture);
 
-    this->sprite.setPosition(x, y);
-    this->sprite.setScale(sf::Vector2f(1.f, 0.4f));
-    this->sprite.setPosition(this->sprite.getPosition().x / 2 + 270,
-                             this->sprite.getPosition().y / 2
-                             );
+//    this->sprite.setPosition(x, y);
+    this->sprite.setScale(sf::Vector2f(0.5f, 0.5f));
+//    sf::Vector2<unsigned int> spriteSize = this->sprite.getTexture()->getSize();
+    sf::Vector2f spriteSize;
+    spriteSize.x = float(this->sprite.getTexture()->getSize().x);
+    this->sprite.setPosition(x - spriteSize.x/4, y / 2);
 
     this->font.loadFromFile(pathFont);
-    this->text.setString(text);
-    this->text.setCharacterSize(24);
-    this->text.setFillColor(sf::Color::White);
-    this->text.setPosition(
-            this->text.getPosition().x / 2 + 270,
-            this->sprite.getPosition().y / 2.f - this->text.getPosition().y / 2.f
-    );
+//    this->text.setString(text);
+//    this->text.setCharacterSize(30);
+//    this->text.setFillColor(sf::Color::White);
+//    this->text.setPosition(
+//            this->sprite.getPosition().x + this->sprite.getTexture()->getSize().x / 2 - this->text.,
+//            this->sprite.getPosition().y / 2.f - this->text.getPosition().y / 2.f
+//    );
 }
 
 GUI::Button::~Button() {}
--- src/menu/menu.cpp
+++ src/menu/menu.cpp
@@ -7,13 +7,13 @@
 
 Menu::Menu::Menu(float width, float height) {
 
-    GUI::Button button1 = GUI::Button(width / 2, height / (3 + 1) * 1, "play", "../assets/font/GorgeousPixel.ttf", "../assets/buttons/Button_Dark.png", "../assets/buttons/Button_Dark_Pressed.png");
+    GUI::Button button1 = GUI::Button(width / 2, height / (3 + 1) * 1, "play", "../assets/font/GorgeousPixel.ttf", "../assets/buttons/play.png", "../assets/buttons/Button_Dark_Pressed.png");
     buttons.push_back(button1);
 
-    GUI::Button button2 = GUI::Button(width / 2, height / (3 + 1) * 2, "options", "../assets/font/GorgeousPixel.ttf", "../assets/buttons/Button_Dark.png", "../assets/buttons/Button_Dark_Pressed.png");
+    GUI::Button button2 = GUI::Button(width / 2, height / (3 + 1) * 2, "options", "../assets/font/GorgeousPixel.ttf", "../assets/buttons/options.png", "../assets/buttons/Button_Dark_Pressed.png");
     buttons.push_back(button2);
 
-    GUI::Button button3 = GUI::Button(width / 2, height / (3 + 1) * 3, "exit", "../assets/font/GorgeousPixel.ttf", "../assets/buttons/Button_Dark.png", "../assets/buttons/Button_Dark_Pressed.png");
+    GUI::Button button3 = GUI::Button(width / 2, height / (3 + 1) * 3, "exit", "../assets/font/GorgeousPixel.ttf", "../assets/buttons/exit.png", "../assets/buttons/Button_Dark_Pressed.png");
     buttons.push_back(button3);
 
     selectItemIndex = 0;
