4f686780bf892dab408c00f8b741bfa21e1d5041
Nikita Schelkanov <nik.schelkanov@gmail.com>
2021-12-16 21:33:01 +0300

    fix code formating

--- network/src/Client/Client.cpp
+++ network/src/Client/Client.cpp
@@ -42,48 +42,46 @@ namespace network {
                     auto msg = Incoming().pop_front().msg;
 
                     switch (msg.header.id) {
-                    case(GameMsg::Client_Accepted): {
-                        std::cout << "Server accepted client - you're in!\n";
-                        message<GameMsg> msg;
-                        msg.header.id = GameMsg::Client_RegisterWithServer;
-                        descPlayer.vPos = { 3.0f, 3.0f };
-                        msg << descPlayer;
-                        Send(msg);
-                        break;
-                    }
-
-                    case(GameMsg::Client_AssignID): {
-                        msg >> nPlayerID;
-                        std::cout << "Assigned Client ID = " << nPlayerID << "\n";
-                        break;
-                    }
-
-                    case(GameMsg::Game_AddPlayer): {
-                        sPlayerDescription desc;
-                        msg >> desc;
-                        mapObjects.insert_or_assign(desc.nUniqueID, desc);
-
-                        if (desc.nUniqueID == nPlayerID) {
-                            bWaitingForConnection = false;
+                        case(GameMsg::Client_Accepted): {
+                            std::cout << "Server accepted client - you're in!\n";
+                            message<GameMsg> msg;
+                            msg.header.id = GameMsg::Client_RegisterWithServer;
+                            descPlayer.vPos = { 3.0f, 3.0f };
+                            msg << descPlayer;
+                            Send(msg);
+                            break;
                         }
-                        break;
-                    }
 
-                    case(GameMsg::Game_RemovePlayer): {
-                        uint32_t nRemovalID = 0;
-                        msg >> nRemovalID;
-                        mapObjects.erase(nRemovalID);
-                        break;
-                    }
+                        case(GameMsg::Client_AssignID): {
+                            msg >> nPlayerID;
+                            std::cout << "Assigned Client ID = " << nPlayerID << "\n";
+                            break;
+                        }
 
-                    case(GameMsg::Game_UpdatePlayer): {
-                        sPlayerDescription desc;
-                        msg >> desc;
-                        mapObjects.insert_or_assign(desc.nUniqueID, desc);
-                        break;
-                    }
+                        case(GameMsg::Game_AddPlayer): {
+                            sPlayerDescription desc;
+                            msg >> desc;
+                            mapObjects.insert_or_assign(desc.nUniqueID, desc);
 
+                            if (desc.nUniqueID == nPlayerID) {
+                                bWaitingForConnection = false;
+                            }
+                            break;
+                        }
 
+                        case(GameMsg::Game_RemovePlayer): {
+                            uint32_t nRemovalID = 0;
+                            msg >> nRemovalID;
+                            mapObjects.erase(nRemovalID);
+                            break;
+                        }
+
+                        case(GameMsg::Game_UpdatePlayer): {
+                            sPlayerDescription desc;
+                            msg >> desc;
+                            mapObjects.insert_or_assign(desc.nUniqueID, desc);
+                            break;
+                        }
                     }
                 }
             }
